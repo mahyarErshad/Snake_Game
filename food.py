a29f3195d6422738f6c49ee09994cda4c5d0409c
mahyarErshad <mahyar.ershad92@gmail.com>
2023-10-15 15:10:06 +0330

    Added Food class

---
+++ food.py
@@ -0,0 +1,15 @@
+from turtle import  Turtle
+import random
+
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        super().shape("circle")
+        super().penup()
+        super().shapesize(stretch_wid=0.5, stretch_len=0.5)
+        super().color("blue")
+        super().speed("fastest")
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        super().goto(random_x, random_y)
